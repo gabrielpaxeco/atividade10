16aef0e3673bd8a0fdaa5800885d01c4cee72925
gabriel pacheco <gabrielenaty0923@gmail.com>
2024-05-20 18:41:04 +0000

    primeiro commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {HomeContainer} from "./Home.styles"
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <h1>MEUS OVOOS</h1>
+    </HomeContainer>
+    
+  )
+}
---
+++ src/pages/Home.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+import React from 'react'
+
+export const HomeContainer = styled.div`
+    h1{
+        color: red;
+    }
+`
